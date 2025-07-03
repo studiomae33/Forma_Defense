71b44ef61f9a145b118b13afe46c5215015951b0
Antoine AUGIS <contact@studiomae.fr>
2025-07-03 23:17:31 +0200

    modif formation lien header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -66,7 +66,7 @@ function MobileNavigation() {
         transition
         className="absolute inset-x-0 top-full mt-4 flex origin-top flex-col rounded-2xl bg-white p-4 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5 data-closed:scale-95 data-closed:opacity-0 data-enter:duration-150 data-enter:ease-out data-leave:duration-100 data-leave:ease-in"
       >
-        <MobileNavLink href="formation">La formation</MobileNavLink>
+                <MobileNavLink href="/Formation">La formation</MobileNavLink>
         <MobileNavLink href="#avis">Les avis</MobileNavLink>
         <hr className="m-2 border-slate-300/40" />
         <MobileNavLink href="Formation">Découvrir la formation</MobileNavLink>
@@ -85,7 +85,7 @@ export function Header() {
               <Logo className="h-10 w-auto" />
             </Link>
             <div className="hidden md:flex md:gap-x-6">
-              <NavLink href="formation">La formation</NavLink>
+                            <NavLink href="/Formation">La formation</NavLink>
               <NavLink href="#avis">Les avis</NavLink>
             </div>
           </div>
